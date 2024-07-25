a578ac316cfe3a40e70d658a52a3e989e513c007
suynnn <suy00065@gmail.com>
2024-07-25 22:33:48 +0900

    chore: 초기설정

---
+++ src/main/java/com/example/nginxjenkinsawstest/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.nginxjenkinsawstest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/check")
+    public String heathCheck() {
+        return "check";
+    }
+}
